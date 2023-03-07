281304f0b09ca2c233c5cd048251e09a25857ba0
balabala <inthe2021@hotmail.com>
2023-03-07 17:17:45 +0800

    update scripts and desc

--- ps.sh
+++ ps.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#20201215
+#20230307
 ps -ef | grep -v grep | grep auto_restart.py
+#
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#20201215
+#20230307
 ps -ef | grep 'auto_restart.py' | grep -v grep | awk {'print $2'} | xargs kill -9
